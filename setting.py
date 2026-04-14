abe0b7366b3a21b30c0150e49549518fbf1bb279
George Sharov <113103652+Joteero@users.noreply.github.com>
2026-04-14 06:14:05 +0000

    first uplload

---
+++ setting.py
@@ -0,0 +1,27 @@
+## sbs / settings.py
+
+### signature first commit
+
+# тут будут храниться все ссылки и логины для тестов
+new_url = "https://task_nimda.ctst.sberins.ru/"
+release_url = "https://release_url.sberins.ru/"
+stage_url = "https://sber-stage.sbps.ru/"
+test_url = "https://sber-test.sbps.ru/"
+psi_url = "http://test.sberins.ru/"
+if_url = "https://test.01.sberins.ru/"
+email = "arjadiy.parovozov@sberins.ru"
+password = "228322"
+email_admin = "miss_kate@sbs.com"
+password_admin = "baltika_9"
+email_2 = "maria.gorilla@sberins.ru"
+password_2 = "baltika_0!"
+
+# StaticMethod
+def wait_for_ajax(type, _locator, timeout=5):
+    """
+    Message = "Element %s was not visible in %s second(s)." % (_locator, str(timeout))
+    driver = SeleniumWebDriver()
+    wait = WebDriverWait(driver, timeout)
+    wait.until(EC.element_to_be_clickable(_type, _locator))
+    # and driver.execute_script("return $.active") == 0, message=message
+```
